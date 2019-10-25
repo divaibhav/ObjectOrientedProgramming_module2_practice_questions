1d3122a9f1f635d46a6399606ff9d5807b42824e
dbc2201 <divyanshb22@gmail.com>
2019-10-25 18:48:56 +0530

    Exception Handling, Question 4: add the throws clause to the main method

--- src/src/exceptionHandling/question4/Main.java
+++ src/src/exceptionHandling/question4/Main.java
@@ -3,12 +3,13 @@ package src.exceptionHandling.question4;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
         //  import the Scanner class and create an object
         final Scanner scanner = new Scanner(System.in);
         //  input a String from the user
         String stringValue = scanner.nextLine();
         //  close the scanner reference
         scanner.close();
+
     }
 }
