7c692041d80aa9d8cd7bd393df18336d2c5e1376
dbc2201 <divyanshb22@gmail.com>
2019-10-25 18:51:41 +0530

    Exception Handling, Question 4: convert the string to a character array

--- src/src/exceptionHandling/question4/Main.java
+++ src/src/exceptionHandling/question4/Main.java
@@ -12,6 +12,8 @@ public class Main {
         scanner.close();
         try {
             int counter = 0;
+            String str;
+            char[] characters = stringValue.toCharArray();
         } catch (ArrayIndexOutOfBoundsException ae) {
             System.out.println("Array Indices Went Out Of Bounds!");
         }
