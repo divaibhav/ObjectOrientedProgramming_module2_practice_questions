b09bfa4710cd2fe1d3a491972c081a49de49b694
dbc2201 <divyanshb22@gmail.com>
2019-10-25 18:38:02 +0530

    Exception Handling, Question 4: import the Scanner class and create an object

--- src/src/exceptionHandling/question4/Main.java
+++ src/src/exceptionHandling/question4/Main.java
@@ -1,7 +1,11 @@
 package src.exceptionHandling.question4;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //  import the Scanner class and create an object
+        final Scanner scanner = new Scanner(System.in);
 
     }
 }
