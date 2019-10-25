d405076475c186ce92485383ef6cad2c73bf7e34
dbc2201 <divyanshb22@gmail.com>
2019-10-25 17:56:16 +0530

    check if the user entered any command line args

--- src/src/collections/question2/Main.java
+++ src/src/collections/question2/Main.java
@@ -4,4 +4,10 @@ package src.collections.question2;
  * This class will input a command line argument from the user and will display array index out of bounds
  */
 public class Main {
+    public static void main(String[] args) {
+        // TODO check if the user entered any command line argument
+        if (args.length < 0) {
+
+        }
+    }
 }
