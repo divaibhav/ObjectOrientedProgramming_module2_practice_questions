a6f3bb9c3c15d859d8e6751d335c29f294352fad
divaibhav <divaibhav@gmail.com>
2019-10-31 15:28:04 +0530

    Created Name class with two fields

---
+++ src/generic/question1/Name.java
@@ -0,0 +1,28 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 31-Oct-19
+ *  Time: 11:01 AM
+ */
+package generic.question1;
+
+public class Name {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
