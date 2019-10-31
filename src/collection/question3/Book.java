bbab62095a325482b7acbb8e6515d95baaf1f943
divaibhav <divaibhav@gmail.com>
2019-10-31 18:01:45 +0530

    Created collection.question3.Solution3 class and Book class (custom class)

---
+++ src/collection/question3/Book.java
@@ -0,0 +1,12 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 31-Oct-19
+ *  Time: 5:59 PM
+ */
+package collection.question3;
+//custom Book class
+public class Book {
+
+
+}
---
+++ src/collection/question3/Solution3.java
@@ -0,0 +1,18 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 31-Oct-19
+ *  Time: 5:59 PM
+ */
+/*
+Create a List of 10 Book objects, where Book is a user defined or custom class.
+Book class contains following field String bookName, String author, long isbn, & double price.
+Book class must be properly encapsulated and must implement data hiding.
+Book class contains parameterized constructors and no-argument constructor.
+Book class must override String toString() of Object class to represent Book object as string
+while printing. Kindly sort the list of book by price.
+ */
+package collection.question3;
+
+public class Solution3 {
+}
