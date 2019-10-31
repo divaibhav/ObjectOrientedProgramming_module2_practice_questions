1d8e3c6a01a607f38d3dc1ba0b0b71d9961576b1
divaibhav <divaibhav@gmail.com>
2019-10-31 15:31:46 +0530

    Created collection.question1.Solution1 class. This is solution of question1

---
+++ src/collection/question1/Solution1.java
@@ -0,0 +1,18 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 31-Oct-19
+ *  Time: 3:29 PM
+ */
+/*
+Create a List & Set of 10 integers and demonstrate ordered and unordered collection as well as
+duplicate allowed and not allowed. Demonstrate addAll() method also.
+
+ */
+package collection.question1;
+
+public class Solution1 {
+    public static void main(String[] args) {
+
+    }
+}
