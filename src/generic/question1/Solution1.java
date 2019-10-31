a21915c13325b3dde5417b56170b8b7b0e382d58
divaibhav <divaibhav@gmail.com>
2019-10-31 15:28:50 +0530

    Created Solution1 class and invoked two generic objects of Student class

---
+++ src/generic/question1/Solution1.java
@@ -0,0 +1,17 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 31-Oct-19
+ *  Time: 10:59 AM
+ */
+package generic.question1;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        Student<String> s1 = new Student<>();
+
+        Student<Name> s2 = new Student<>();
+
+    }
+
+}
