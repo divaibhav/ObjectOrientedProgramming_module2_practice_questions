ee4954ec614aff2a8c61942789a6c5e235d5040e
divaibhav <divaibhav@gmail.com>
2019-10-31 15:44:40 +0530

    Added 10 elements to integers list and demonstrate autoboxing and boxing

--- src/collection/question1/Solution1.java
+++ src/collection/question1/Solution1.java
@@ -17,6 +17,26 @@ public class Solution1 {
     public static void main(String[] args) {
         //creating a ArrayList of 10 Integers
         ArrayList<Integer> integers = new ArrayList<>();
+        // adding elements in ArrayList by using add(Element e) method
+        // by passing primitive value, which will be converted to object by autoboxing
+        integers.add(20);
+        //creating Integer object to represent primitive value 40 by using valueOf() method
+        Integer obj = Integer.valueOf(40);
+        //adding obj to list
+        integers.add(obj);
+
+        // likewise added 8 more elements in list
+        integers.add(60);
+        integers.add(70);
+        integers.add(50);
+        integers.add(30);
+        integers.add(250);
+        integers.add(3);
+        integers.add(2);
+        integers.add(10);
+
+
+
 
     }
 }
