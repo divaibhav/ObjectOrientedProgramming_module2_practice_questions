9bd3c19a276f167c1a9a663863e1af2e212bb930
divaibhav <divaibhav@gmail.com>
2019-10-31 22:29:13 +0530

    created Student class

---
+++ src/collection/question4/Student.java
@@ -0,0 +1,46 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 31-Oct-19
+ *  Time: 10:27 PM
+ */
+package collection.question4;
+
+public class Student {
+    private String name;
+    private int rollNo;
+    private double cpi;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public double getCpi() {
+        return cpi;
+    }
+
+    public void setCpi(double cpi) {
+        this.cpi = cpi;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rollNo=" + rollNo +
+                ", cpi=" + cpi +
+                '}';
+    }
+}
