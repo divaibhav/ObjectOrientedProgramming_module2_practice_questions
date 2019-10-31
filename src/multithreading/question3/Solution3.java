d0fe2fe8b5d92ece0aa7d0aff6d75323b8327bea
divaibhav <divaibhav@gmail.com>
2019-10-31 23:39:02 +0530

    Created and implemented question3.Solution3

---
+++ src/multithreading/question3/Solution3.java
@@ -0,0 +1,15 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 31-Oct-19
+ *  Time: 11:37 PM
+ */
+package multithreading.question3;
+
+public class Solution3 {
+    public static void main(String[] args) {
+        Thread currentThread = Thread.currentThread();
+        System.out.println(currentThread.getPriority());
+
+    }
+}
