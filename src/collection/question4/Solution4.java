eb99d55611aadfe477837e2068e9a045e8dcd6e9
divaibhav <divaibhav@gmail.com>
2019-10-31 22:27:22 +0530

    created Solution4 class

---
+++ src/collection/question4/Solution4.java
@@ -0,0 +1,13 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 31-Oct-19
+ *  Time: 10:26 PM
+ */
+package collection.question4;
+
+public class Solution4 {
+    public static void main(String[] args) {
+
+    }
+}
