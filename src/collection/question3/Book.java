e6916e676727f24f67b84fc93ed6e9a605082d8a
divaibhav <divaibhav@gmail.com>
2019-10-31 18:05:38 +0530

    implemented Book class as per specification

--- src/collection/question3/Book.java
+++ src/collection/question3/Book.java
@@ -7,6 +7,48 @@
 package collection.question3;
 //custom Book class
 public class Book {
+    //instance fields
+    private String bookName;
+    private String author;
+    private long isbn;
+    private double price;
+    //parameterized constructor
+    public Book(String bookName, String author, long isbn, double price) {
+        this.bookName = bookName;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+    }
+    //no-argument constructor
+    public Book() {
+        super();
+    }
+    //getter and setter
+    public String getBookName() {
+        return bookName;
+    }
 
+    public String getAuthor() {
+        return author;
+    }
 
+    public long getIsbn() {
+        return isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    //overriding toString() method
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn=" + isbn +
+                ", price=" + price +
+                '}';
+    }
 }
