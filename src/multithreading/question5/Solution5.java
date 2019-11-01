576865188017e31d14bf74eca0b08211ce35d248
divaibhav <divaibhav@gmail.com>
2019-11-02 01:28:46 +0530

    implemented question5.Solution5 class

--- src/multithreading/question5/Solution5.java
+++ src/multithreading/question5/Solution5.java
@@ -8,6 +8,16 @@ package multithreading.question5;
 
 public class Solution5 {
     public static void main(String[] args) {
+        Thread t = new Thread();
+        //setting priority to 100
+        t.setPriority(100); //no error at compilation, but generate exception IllegalArgumentException
 
     }
 }
+/*
+Output:
+Exception in thread "main" java.lang.IllegalArgumentException
+	at java.base/java.lang.Thread.setPriority(Thread.java:1142)
+	at multithreading.question5.Solution5.main(Solution5.java:13)
+
+ */
