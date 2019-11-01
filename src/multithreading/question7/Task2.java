d28bb882acd1f4d56d01417fbdcbfadb02084ee0
divaibhav <divaibhav@gmail.com>
2019-11-02 02:00:29 +0530

    updated Task2 for reading input using Scanner class

--- src/multithreading/question7/Task2.java
+++ src/multithreading/question7/Task2.java
@@ -6,14 +6,18 @@
  */
 package multithreading.question7;
 
+import java.util.Scanner;
+
 public class Task2 implements Runnable {
     // task is to print world 10 times with a gap of 2 sec
     @Override
     public void run() {
+        Scanner sc =new Scanner(System.in);
         for (int i = 0; i < 10 ; i++) {
             try {
                 Thread.sleep(2000);
-                System.out.println("world -> ");
+                System.out.print("world -> ");
+                sc.nextLine();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
