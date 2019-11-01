73f2847ad550044308c614567a2805977eecbc08
divaibhav <divaibhav@gmail.com>
2019-11-02 02:36:20 +0530

    created and implemented stringhandling.question1.Solution1 class

---
+++ src/stringhandling/question1/Solution1.java
@@ -0,0 +1,22 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 02-Nov-19
+ *  Time: 2:31 AM
+ */
+package stringhandling.question1;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder("hello");
+        StringBuffer buffer = new StringBuffer("programming");
+
+        // there is no concat method either in StringBuilder or in StringBUffer,
+        // we can use append or String class concat method
+
+        //using String class concat() mehtod
+        String result = builder.toString().concat(buffer.toString());
+
+        System.out.println(result);
+    }
+}
