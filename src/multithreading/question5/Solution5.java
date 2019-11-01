3efb96c1e514d8c620770981e08e06b5917f92fe
divaibhav <divaibhav@gmail.com>
2019-11-02 01:25:18 +0530

    created question5.Solution5 class

---
+++ src/multithreading/question5/Solution5.java
@@ -0,0 +1,13 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 02-Nov-19
+ *  Time: 1:24 AM
+ */
+package multithreading.question5;
+
+public class Solution5 {
+    public static void main(String[] args) {
+
+    }
+}
