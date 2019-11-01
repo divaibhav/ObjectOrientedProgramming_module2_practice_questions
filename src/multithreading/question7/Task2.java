033c5d220183adb8d34cc79dc8febddba038d6a8
divaibhav <divaibhav@gmail.com>
2019-11-02 01:46:33 +0530

    created and Implemented question7.Task2 class

---
+++ src/multithreading/question7/Task2.java
@@ -0,0 +1,22 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 02-Nov-19
+ *  Time: 1:39 AM
+ */
+package multithreading.question7;
+
+public class Task2 implements Runnable {
+    // task is to print world 10 times with a gap of 2 sec
+    @Override
+    public void run() {
+        for (int i = 0; i < 10 ; i++) {
+            try {
+                Thread.sleep(2000);
+                System.out.println("world -> ");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
