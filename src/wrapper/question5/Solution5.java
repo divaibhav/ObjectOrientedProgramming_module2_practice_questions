57773774b1195190bcf7d74a40b3ebc031ac8eb3
divaibhav <divaibhav@gmail.com>
2019-11-03 16:52:00 +0530

    created and implemented wrapper.question5.Solution5

---
+++ src/wrapper/question5/Solution5.java
@@ -0,0 +1,14 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 03-Nov-19
+ *  Time: 4:43 PM
+ */
+package wrapper.question5;
+
+public class Solution5 {
+    public static void main(String[] args) {
+        Character obj = Character.valueOf('A');
+        System.out.println((int) obj);
+    }
+}
