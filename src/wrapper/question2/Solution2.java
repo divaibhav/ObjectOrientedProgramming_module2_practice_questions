855359193fed1996c7db68d6e9a14ec951961e87
divaibhav <divaibhav@gmail.com>
2019-11-03 16:26:10 +0530

    created and implemented wrapper.question2.Solution2

---
+++ src/wrapper/question2/Solution2.java
@@ -0,0 +1,18 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 03-Nov-19
+ *  Time: 4:23 PM
+ */
+package wrapper.question2;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        //boxing
+        Integer objInteger = Integer.valueOf(10);
+        //un-boxing
+        int a = objInteger.intValue();
+
+        System.out.println(a);
+    }
+}
