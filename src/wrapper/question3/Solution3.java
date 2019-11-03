81d82dd275432aeeec29b51c9a2b5a2ba472b45a
divaibhav <divaibhav@gmail.com>
2019-11-03 16:31:32 +0530

    created and implemented wrapper.question3.Solution3

---
+++ src/wrapper/question3/Solution3.java
@@ -0,0 +1,19 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 03-Nov-19
+ *  Time: 4:29 PM
+ */
+package wrapper.question3;
+
+public class Solution3 {
+    public static void main(String[] args) {
+        //autoboxing
+        Integer objInteger = 10;
+        //un-boxing
+        int a = objInteger;
+
+        System.out.println(a);
+
+    }
+}
